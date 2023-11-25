2ddcd24d96496eeb42d75c758b38f92930773495
Okta Prianto <oktaprianto10@gmail.com>
2023-11-25 21:08:59 +0700

    hapus semua

--- README.md
+++
@@ -1,1 +0,0 @@
-# capstone_dicoding_jakarta
--- backend/routes/tours.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createTour } from "../controllers/tourControllers.js";
-
-const router = express.Router();
-
-router.post('/', createTour);
-
-export default router;
